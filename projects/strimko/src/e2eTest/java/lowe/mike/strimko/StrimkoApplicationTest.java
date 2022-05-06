ba767c5434090e51aadf35ee640b1a51a6bd50a4
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2022-05-06 18:11:05 +0100

    Headless testing setup

--- projects/strimko/src/e2eTest/java/lowe/mike/strimko/StrimkoApplicationTest.java
+++ projects/strimko/src/e2eTest/java/lowe/mike/strimko/StrimkoApplicationTest.java
@@ -9,6 +9,18 @@ import org.testfx.framework.junit5.ApplicationTest;
  */
 public class StrimkoApplicationTest extends ApplicationTest {
 
+  /**
+   * Headless testing set up.
+   */
+  @BeforeAll
+  public static void headlessSetUp() {
+    System.setProperty("java.awt.headless", "true");
+    System.setProperty("testfx.headless", "true");
+    System.setProperty("testfx.robot", "glass");
+    System.setProperty("prism.order", "sw");
+    System.setProperty("prism.text", "t2k");
+  }
+
   @Override
   public void start(Stage stage) {
     new StrimkoApplication().start(stage);
