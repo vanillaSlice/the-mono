4af1f164f3d0c3f1f6668bc52b83bdb66c7a9056
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2023-12-08 15:19:01 +0000

    Advent of code 2023 day 8

---
+++ challenges/advent-of-code/2023/src/day8a.ts
@@ -0,0 +1,31 @@
+import { readInput } from './util.ts';
+
+export const countSteps = (input: string): number => {
+  const lines = input.split(/\r?\n/).filter(line => line);
+
+  const instructions = lines[0];
+
+  const nodes = lines.slice(1).reduce((acc, line) => {
+    const groups = /(?<node>.*) = \((?<left>.*), (?<right>.*)\)/.exec(line)!.groups!;
+    acc[groups.node] = [groups.left, groups.right];
+    return acc;
+  }, {} as Record<string, string[]>);
+
+  let steps = 0;
+  let currentLocation = 'AAA';
+
+  while (true) {
+    const currentStep = instructions[steps % instructions.length];
+    const nextLocations = nodes[currentLocation];
+    currentLocation = currentStep === 'L' ? nextLocations[0] : nextLocations[1];
+    steps++;
+    if (currentLocation === 'ZZZ') {
+      return steps;
+    }
+  }
+};
+
+if (Deno.args && Deno.args[0] == 'solve') {
+  const text = await readInput('day8a.txt');
+  console.log(countSteps(text));
+}
